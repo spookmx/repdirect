68bf15e5c900b0b242c1f2a94b6bfe602a38b20a
Jose Romero <spookmx@gmail.com>
2016-11-02 08:36:17 -0500

    Progress

--- imports/ui/components/partiesList/partiesList.js
+++ imports/ui/components/partiesList/partiesList.js
@@ -1,7 +1,11 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
 
 import template from './partiesList.html';
+import { Parties } from '../../../api/parties';
+import { name as PartyAdd } from '../partyAdd/partyAdd';
+import { name as PartyRemove } from '../partyRemove/partyRemove';
 
 class PartiesList {
   constructor($scope, $reactive) {
@@ -21,9 +25,21 @@ const name = 'partiesList';
 
 // create a module
 export default angular.module(name, [
-  angularMeteor
+  angularMeteor,
+  uiRouter,
+  PartyAdd,
+  PartyRemove
 ]).component(name, {
   template,
   controllerAs: name,
   controller: PartiesList
-});
+}).config(config);
+
+function config($stateProvider) {
+  'ngInject';
+  $stateProvider
+    .state('parties', {
+      url: '/parties',
+      template: '<parties-list></parties-list>'
+    });
+}
---
+++ imports/ui/components/partyDetails/partyDetails.js
@@ -0,0 +1,59 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
+
+import template from './partyDetails.html';
+import { Parties } from '../../../api/parties';
+
+class PartyDetails {
+  constructor($stateParams, $scope, $reactive) {
+    'ngInject';
+
+    $reactive(this).attach($scope);
+
+    this.partyId = $stateParams.partyId;
+    this.helpers({
+      party(){
+        return Parties.findOne({_id: $stateParams.partyId});
+      }
+    });
+  }
+
+  save() {
+    Parties.update({
+      _id: this.party._id
+    }, {
+      $set: {
+        name: this.party.name,
+        description: this.party.description
+      }
+    }, (error) => {
+      if (error) {
+        console.log('Oops, unable to update the party...');
+      } else {
+        console.log('Done!');
+      }
+    });
+  }
+}
+
+const name = 'partyDetails';
+
+// create a module
+export default angular.module(name, [
+  angularMeteor,
+  uiRouter
+]).component(name, {
+  template,
+  controllerAs: name,
+  controller: PartyDetails
+}).config(config);
+
+function config($stateProvider) {
+  'ngInject';
+
+  $stateProvider.state('partyDetails', {
+    url: '/parties/:partyId',
+    template: '<party-details></party-details>'
+  });
+}
